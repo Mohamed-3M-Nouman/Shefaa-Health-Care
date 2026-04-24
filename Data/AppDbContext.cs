61e5329738249dc80fbe6d5f69e247b456fe3f78
Saif Aldeen Wael Alsayed <seafeldeenwael@gmail.com>
2026-04-24 17:49:12 +0300

    Initial commit: Setup ShefaaHealthCare project and EF Core configurations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ShefaaHealthCare.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
